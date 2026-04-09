592d21229022eaf1d974fd6e56de62bb8e6b5416
Linn Thit Oo <linnthitoo@Linns-MacBook-Pro.local>
2026-04-09 13:46:05 +0630

    message

--- app/src/main/java/uk/ncc/fitNova/data/remote/BackendConfig.kt
+++ app/src/main/java/uk/ncc/fitNova/data/remote/BackendConfig.kt
@@ -1,7 +1,7 @@
 package uk.ncc.fitNova.data.remote
 
 object BackendConfig {
-    private const val BASE_URL = "http://192.168.1.4:8000"
+    private const val BASE_URL = "http://10.0.2.2/backend"
 
     const val LOGIN_URL = "$BASE_URL/LoginDAO.php"
     const val REGISTRATION_URL = "$BASE_URL/RegistrationDAO.php"
